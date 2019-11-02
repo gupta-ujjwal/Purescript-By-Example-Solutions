f890c4077b0480151d6e1d946b969f4ef847f0bb
Ujjwal Gupta <ujjwal.gupta.16csc@bml.edu.in>
2019-11-02 15:56:06 +0530

    Ex3 chap6 completed

--- Chap6/src/Main.purs
+++ Chap6/src/Main.purs
@@ -31,3 +31,41 @@ data NonEmpty a = NonEmpty a (Array a)
 
 instance showNonEmpty :: (Show a) => Show (NonEmpty a) where
   show (NonEmpty a ax) = show (a:ax) 
+
+instance eqNonEmpty :: (Eq a, Eq (Array a)) => Eq (NonEmpty a) where
+  eq (NonEmpty a ax) (NonEmpty b bx) = (a==b)&&(ax==bx)
+
+instance semiGroupNonEmpty :: Semigroup (NonEmpty a) where
+  append (NonEmpty a ax) (NonEmpty b bx) = (NonEmpty a (ax <> (b:bx)))
+
+instance functorNonEmpty :: Functor (NonEmpty) where
+  map f (NonEmpty a ax) = (NonEmpty (f a) (map f ax))
+
+data Extended a = Finite a | Infinite
+
+equalsExtended :: forall a. (Eq a) => (Extended a) -> (Extended a) -> Boolean
+equalsExtended (Finite x) (Finite y) = eq x y
+equalsExtended (Finite x) Infinite = false
+equalsExtended Infinite (Finite y) = false
+equalsExtended Infinite Infinite = true
+
+notEqualsExtended :: forall a. (Eq a) => (Extended a) -> (Extended a) -> Boolean
+notEqualsExtended e e' = not $ equalsExtended e e'
+
+instance eqExtended :: (Eq a) => Eq (Extended a) where
+  eq e e' = equalsExtended e e'
+  
+orderExtended :: forall a. (Ord a) => Extended a-> Extended a-> Ordering
+orderExtended (Finite x) (Finite y) = compare x y
+orderExtended (Finite x) (Infinite) = LT
+orderExtended Infinite (Finite y) = GT
+orderExtended Infinite Infinite = EQ
+
+instance ordExtended :: (Ord a) => Ord (Extended a) where
+  compare e e' = orderExtended e e'
+
+
+instance foldableNonEmpty :: Foldable NonEmpty where
+  foldr f a (NonEmpty x xs) = foldr f a (x:xs)
+  foldl f a (NonEmpty x xs) = foldl f a (x:xs)
+  foldMap f (NonEmpty x xs) = foldMap f (x:xs)
